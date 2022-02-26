c3f54c8b068d08d5ffd8f9842fb7af6c23403b7f
Viduvern <victor.duvernois2@gmail.com>
2022-02-26 16:51:18 +0100

    Give owner when leave game

--- back/src/web/in/LeaveGameIn.js
+++ back/src/web/in/LeaveGameIn.js
@@ -8,7 +8,19 @@ class LeaveGameIn {
         socket.on("leaveGame", (data) => {
             let game = this.gameService.getGame(data.gameId);
             game.disconnect(socket);
-            // We must add an if statement if the player that leave is Owner of the game and change it
+            // Check If The player that leave the game is Owner and is not alone, if its the case give the owner to someone else
+            if(Object.keys(game.players).length > 1){
+                game.players.map(player => {
+                    if(player.id === data.playerId && player.isOwner){
+                        for (var i = 0; i < Object.keys(game.players).length; i++) {
+                            if (!game.players[i].isOwner) {
+                                game.players[i].isOwner = true
+                                break;
+                            }
+                        }       
+                    }
+            })
+        }
             game.players = game.players.filter(player => player.id !== data.playerId);
             this.gameService.saveGame(game);
             this.gameOut.refreshGame(game);
