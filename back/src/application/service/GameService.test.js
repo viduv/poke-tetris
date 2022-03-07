55952bf279fe2808428e49214f72f5ff49c1ffbc
Viduvern <victor.duvernois2@gmail.com>
2022-03-07 17:15:31 +0100

    Add some test

--- back/src/application/service/GameService.test.js
+++ back/src/application/service/GameService.test.js
@@ -8,7 +8,7 @@ test('add a Game', () => {
 	expect(gameServ.games.length).toBe(1);
 })
 
-test(' get Game by id', () => {
+test('get Game by id', () => {
 	gameServ = new GameService
 	gameServ.addGame("antoineGame", "antoine", true, { id: "555", name: "socket"})
 	game = gameServ.addGame("etienneGame", "etienne", true, { id: "555", name: "socket"})
@@ -16,4 +16,27 @@ test(' get Game by id', () => {
 	expect(gameServ.getGame("idthatnotexist")).toStrictEqual("this game does not exit or your url is not correctly formated")
 	game1 = gameServ.getGame(game.id)
 	expect(game1.id).toBe(game.id);
+})
+
+test('get Games', () => {
+	gameServ = new GameService
+	gameServ.addGame("antoineGame", "antoine", true, { id: "555", name: "socket"})
+	expect(gameServ.getGames().length).toBe(1)
+})
+
+test('Delete Game', () => {
+	gameServ = new GameService
+	gameServ.addGame("antoineGame", "antoine", true, { id: "555", name: "socket"})
+	game = gameServ.addGame("etienneGame", "etienne", true, { id: "555", name: "socket"})
+	expect(gameServ.games.length).toBe(2);
+	gameServ.deleteGame(game.id)
+	expect(gameServ.games.length).toBe(1);
+})
+
+test('Test If User is in the Game', () => {
+	gameServ = new GameService
+	gameServ.addGame("antoineGame", "antoine", true, { id: "555", name: "socket"})
+	game = gameServ.addGame("etienneGame", "etienne", true, { id: "554", name: "socket"})
+	expect(gameServ.checkGameUser("badId")).toStrictEqual("FALSE")
+	expect(gameServ.checkGameUser("554").id).toBe(game.id)
 })
