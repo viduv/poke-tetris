a7e61ca9caff918e8757cf5daf4aa1d6425b9db3
viduv <victor.duvernois2@gmail.com>
2022-05-03 16:59:46 +0200

    clean front test

--- front/src/test.ts
+++ front/src/test.ts
@@ -1,11 +1,20 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
 import 'zone.js/testing';
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import {Socket, SocketIoModule} from "ngx-socket-io";
+import { AppComponent } from './app/app.component';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
+import {MatDialogModule,  MatDialogRef} from '@angular/material/dialog';
+import { preGameReducer } from './app/stores/pre-game-store/pre-game.reducer';
+import { StoreModule } from '@ngrx/store';
+import { GameReducer } from './app/stores/game-store/game.reducer';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 
 declare const require: {
   context(path: string, deep?: boolean, filter?: RegExp): {
@@ -20,6 +29,26 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting(),
 );
 
+beforeEach(async () => {
+  await TestBed.configureTestingModule({
+    imports: [
+      RouterTestingModule,
+      StoreModule.forRoot({ preGame: preGameReducer, Game : GameReducer}, {}),
+      SocketIoModule.forRoot({ url: 'http://localhost:4444', options: {} }),
+      MatDialogModule,
+      MatSnackBarModule,
+    ],
+    declarations: [
+      AppComponent
+    ],
+    providers: [
+      {
+        provide: MatDialogRef,
+        useValue: {}
+      }
+    ]
+  }).compileComponents();
+});
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
