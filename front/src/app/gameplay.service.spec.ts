45bd749d1960620229cee2bff4364e893a92671e
Felix PAGE <fpage@e2r11p2.clusters.42paris.fr>
2022-05-04 13:17:12 +0200

    start gameplay test

--- front/src/app/gameplay.service.spec.ts
+++ front/src/app/gameplay.service.spec.ts
@@ -13,4 +13,25 @@ describe('GameplayService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should be initialize', () => {
+    let spySpawnNewPiece = spyOn((<any>service), "spawnNewPiece").and.callFake(() => {});
+    let spyDrawPiece = spyOn((<any>service), "drawPiece").and.callFake(() => {});
+
+    service.start();
+    expect((<any>service).gridSize).toEqual({height: 20, width: 10});
+    expect((<any>service).gameSpeed).toEqual(300);
+    expect((<any>service).isLose).toBeFalse();
+    expect((<any>service).locked).toBeFalse();
+    expect((<any>service).grid.length).toEqual(10 * 20);
+    service.grid.forEach(tile => {
+      expect(tile.solid).toBeFalse();
+      expect(tile.color).toEqual("transparent");
+    });
+    expect((<any>service).isRun).toBeTrue();
+    expect(spySpawnNewPiece);
+    expect(spyDrawPiece);
+    expect((<any>service).spawnNewPiece).toHaveBeenCalled();
+    expect((<any>service).drawPiece).toHaveBeenCalled();
+  });
 });
